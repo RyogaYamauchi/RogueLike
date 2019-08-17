2bf30914c3c54538990a12ee094b1edd945d918e
Ryoga_Yamauchi <g1854003@cc.kyoto-su.ac.jp>
2019-08-17 17:03:46 +0900

    enemyの移動を直接座標指定じゃなくICharactorのMoveに渡して処理した

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -13,15 +13,16 @@ namespace Scripts
     {
         public Vector2Int TargetPosition;
         Queue<Vector2Int> RootQueue;
+
         public void Spawn(int id)
         {
             Id = id;
             var roomId = Random.Range(1, GameController.Instance.field.RoomsDictionary.Count);
             if (roomId == GameController.Instance.player.CurrentRoomId) Spawn(id);
             CurrentRoomId = roomId;
-            var room = GameController.Instance.field.RoomsDictionary[roomId]; 
-            var x = Random.Range(room.X, room.X + room.XRange); 
-            var y = Random.Range(room.Y, room.Y + room.YRange); 
+            var room = GameController.Instance.field.RoomsDictionary[roomId];
+            var x = Random.Range(room.X, room.X + room.XRange);
+            var y = Random.Range(room.Y, room.Y + room.YRange);
             Position = new Vector2Int(x, y);
             RootQueue = new Queue<Vector2Int>();
             TargetPosition = RandomPosition(CurrentRoomId);
@@ -33,10 +34,8 @@ namespace Scripts
             {
                 TargetPosition = GameController.Instance.player.Position;
             }
-            // TODO : ルート上に何もなく通れる時に再計算しないように
+
             RootQueue = GetRoute();
-            
-            if(RootQueue.Count == 0){Debug.Log("バグ");}
 
             //同じ部屋にプレイヤーがいるとき
             if (RootQueue.Peek() == GameController.Instance.player.Position)
@@ -47,8 +46,7 @@ namespace Scripts
             //部屋にプレイヤーがいない時
             else
             {
-                Debug.Log($"今が{Position}, ターゲットが {TargetPosition} , 移動するぜ！ {RootQueue.Peek()}");
-                Position = RootQueue.Dequeue();
+                Move(RootQueue.Peek());
                 if (RootQueue.Count == 0)
                 {
                     TargetPosition = RandomPosition(CurrentRoomId);
@@ -57,6 +55,14 @@ namespace Scripts
 
             yield return new WaitForSeconds(0.5f);
             action?.Invoke();
+        }
+
+        private void CorrectCallBack(int dx, int dy)
+        {
+            GameController.Instance.field.Cells.ArrayCells2D[Position.x, Position.y].OnState =
+                MasterFieldOnState.None;
+            GameController.Instance.field.Cells.ArrayCells2D[Position.x + dx, Position.y + dy].OnState =
+                MasterFieldOnState.Enemy;
         }
 
         private Queue<Vector2Int> GetRoute()
@@ -82,9 +88,64 @@ namespace Scripts
 
         public bool Die()
         {
-//            var obj = GameObjecw.Find(Name + "(Clone)");
             Destroy(gameObject);
             return true;
+        }
+
+        public void Move(Vector2Int nextPosition)
+        {
+            var dx = nextPosition.x - Position.x;
+            var dy = nextPosition.y - Position.y;
+            Debug.Log($"今のポジション : {Position} , 行きたいのは{RootQueue.Peek()}");
+            Debug.Log($"dx : {dx} , dy : {dy} ");
+            
+            if (dx == 1 && dy == 1) //upperright
+            {
+                StartCoroutine(UpperRight(() => { CorrectCallBack(1, 1); }, () => { }));
+                RootQueue.Dequeue();
+            }
+
+            else if (dx == -1 && dy == 1) //upperleft
+            {
+                StartCoroutine(UpperLeft(() => { CorrectCallBack(-1, 1); }, () => { }));
+                RootQueue.Dequeue();
+            }
+
+            else if (dx == -1 && dy == -1) //bottomleft
+            {
+                StartCoroutine(BottomLeft(() => { CorrectCallBack(-1, -1); }, () => { }));
+                RootQueue.Dequeue();
+            }
+
+            else if (dx == 1 && dy == -1) //bottomright
+            {
+                StartCoroutine(BotttomRight(() => { CorrectCallBack(1, -1); }, () => { }));
+                RootQueue.Dequeue();
+            }
+            else if (dx == 1) //right
+            {
+                StartCoroutine(MoveRight(() => { CorrectCallBack(1, 0); }, () => { }));
+                RootQueue.Dequeue();
+            }
+
+            else if (dx == -1) //left
+            {
+                StartCoroutine(MoveLeft(() => { CorrectCallBack(-1, 0); }, () => { }));
+                RootQueue.Dequeue();
+            }
+
+            else if (dy == 1) //up
+            {
+                StartCoroutine(MoveUp(() => { CorrectCallBack(0, 1); }, () => { }));
+                RootQueue.Dequeue();
+            }
+
+            else if (dy == -1) //down
+            {
+                StartCoroutine(MoveDown(() => { CorrectCallBack(0, -1); }, () => { }));
+                RootQueue.Dequeue();
+            }
+
         }
     }
 }
